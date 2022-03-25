d80e05601963a2159504e3c5bdd2359f1dc374ce
Fernando Arimori <fernando.c.arimori@gmail.com>
2022-03-25 12:43:50 -0300

    Routers @03.25.2022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { LogarComponent } from './logar/logar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'entrar', component: LogarComponent},
+  {path: 'cadastrar', component: CadastrarComponent}
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
