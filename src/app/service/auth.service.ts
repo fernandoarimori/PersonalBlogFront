5e0627f359e18a1364759f1af9091241055825cb
Fernando Arimori <fernando.c.arimori@gmail.com>
2022-04-06 22:53:47 -0300

    get postagens (with bugs 🐞)

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient,HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
@@ -14,6 +14,15 @@ export class AuthService {
     private http: HttpClient
   ) { }
 
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token)
+  }
+  refreshToken(){
+    this.token = {
+      headers: new HttpHeaders().set('Authorization', environment.token)
+    }
+  }
+
   entrar(userLogin: UserLogin): Observable<UserLogin>{
     return this.http.post<UserLogin>('https://blogfernando.herokuapp.com/usuarios/logar', userLogin)
   }
@@ -21,6 +30,10 @@ export class AuthService {
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://blogfernando.herokuapp.com/usuarios/cadastrar', user)
+  }
+
+  getByIdUser(id: number): Observable<User>{
+    return this.http.get<User>(`https://blogfernando.herokuapp.com/usuarios/${id}`, this.token)
   }
 
   logado(){
